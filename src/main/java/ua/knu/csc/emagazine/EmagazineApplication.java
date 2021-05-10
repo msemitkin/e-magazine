08a977b2e2e7a1d538cadc5e24cd711d58f5accc
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-05-10 10:39:45 +0300

    initial commit

---
+++ src/main/java/ua/knu/csc/emagazine/EmagazineApplication.java
@@ -0,0 +1,13 @@
+package ua.knu.csc.emagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmagazineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/knu/csc/emagazine/EmagazineApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.knu.csc.emagazine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmagazineApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> {
+        });
+    }
+
+}
